a99582de6fa91b66c7cad7192b3b30a43e9fcae7
kvaettr <170894041+kvaettr@users.noreply.github.com>
2024-07-15 22:42:08 -0600

    Undo last changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,8 @@
-const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+
 
 module.exports = function(eleventyConfig) {
     // Copy `src/style.css` to `_site/style.css`
     eleventyConfig.addPassthroughCopy("src/style.css");
-    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-      // which file extensions to process
-      extensions: "html",
-  
-      // Add any other Image utility options here:
-  
-      // optional, output image formats
-      formats: ["webp", "jpeg"],
-      // formats: ["auto"],
-  
-      // optional, output image widths
-      // widths: ["auto"],
-  
-      // optional, attributes assigned on <img> override these values.
-      defaultAttributes: {
-        loading: "lazy",
-        decoding: "async",
-      },
-    });
-  };
   
 return {
     // When a passthrough file is modified, rebuild the pages:
