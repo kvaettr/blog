2a5952f9123ec4d9a9dfb791001862e843e3888e
kvaettr <170894041+kvaettr@users.noreply.github.com>
2024-08-04 12:19:07 -0600

    Undo what I did with eleventy rss plugin info in eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/style.css");
     eleventyConfig.addPassthroughCopy("src/images/");
     eleventyConfig.addPassthroughCopy("src/pages/");
-  
+
 return {
     // When a passthrough file is modified, rebuild the pages:
     passthroughFileCopy: true,
@@ -14,30 +14,4 @@ return {
       data: "_data",
       output: "_site",
     }
-  };
-};
-
-const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
-
-// If you already have a configuration file make sure you don’t end
-// up with multiple `module.exports =` in the same file!
-module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(feedPlugin, {
-		type: "rss", // or "rss", "json"
-		outputPath: "/feed.xml",
-		collection: {
-			name: "posts", // iterate over `collections.posts`
-			limit: 10,     // 0 means no limit
-		},
-		metadata: {
-			language: "en",
-			title: "Vaettr",
-			subtitle: "Blogging as an aspect of my creative practice and self-education",
-			base: "https://vaettr.com/",
-			author: {
-				name: "Vae",
-				email: "", // Optional
-			}
-		}
-	});
-};
+  }};
