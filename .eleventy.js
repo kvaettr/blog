51ff08849b3c7c138064461cee75d618633642f1
kvaettr <170894041+kvaettr@users.noreply.github.com>
2024-08-27 00:46:54 -0600

    Change eleventy.js config to define markdownIt

--- .eleventy.js
+++ .eleventy.js
@@ -38,25 +38,24 @@ module.exports = function(eleventyConfig) {
 
 // markdownit link changes start here
 
-let markdownLibrary = markdownIt({
-  html: true,
-  breaks: true,
-  linkify: true
-})
+  eleventyConfig.setLibrary("md", markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  })
 
-.use(mdIterator, 'url_new_win', 'link_open', function (tokens, idx) {
-  const [attrName, href] = tokens[idx].attrs.find(attr => attr[0] === 'href')
-  
-  if (href && (!href.includes('vaettr.com') && !href.startsWith('/') && !href.startsWith('#'))) {
-    tokens[idx].attrPush([ 'target', '_blank' ])
-    tokens[idx].attrPush([ 'rel', 'noopener noreferrer' ])
-  }
-}).use(markdownItAnchor, {
-  permalink: true,
-  permalinkClass: "direct-link",
-  permalinkSymbol: "#"
-})
-eleventyConfig.setLibrary("md", markdownLibrary);
+    .use(mdIterator, 'url_new_win', 'link_open', function (tokens, idx) {
+      const [attrName, href] = tokens[idx].attrs.find(attr => attr[0] === 'href');
+
+      if (href && (!href.includes('vaettr.com') && !href.startsWith('/') && !href.startsWith('#'))) {
+        tokens[idx].attrPush(['target', '_blank']);
+        tokens[idx].attrPush(['rel', 'noopener noreferrer']);
+      }
+    }).use(markdownItAnchor, {
+      permalink: true,
+      permalinkClass: "direct-link",
+      permalinkSymbol: "#"
+    }));
 
 //and end here
 
