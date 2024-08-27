7420633466df04e290d59e4234ddcf958deeeda4
kvaettr <170894041+kvaettr@users.noreply.github.com>
2024-08-27 01:22:18 -0600

    Make a big mess and undo it

--- .eleventy.js
+++ .eleventy.js
@@ -36,29 +36,6 @@ module.exports = function(eleventyConfig) {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
 })
 
-// markdownit link changes start here
-
-  eleventyConfig.setLibrary("md", markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  })
-
-    .use(mdIterator, 'url_new_win', 'link_open', function (tokens, idx) {
-      const [attrName, href] = tokens[idx].attrs.find(attr => attr[0] === 'href');
-
-      if (href && (!href.includes('vaettr.com') && !href.startsWith('/') && !href.startsWith('#'))) {
-        tokens[idx].attrPush(['target', '_blank']);
-        tokens[idx].attrPush(['rel', 'noopener noreferrer']);
-      }
-    }).use(markdownItAnchor, {
-      permalink: true,
-      permalinkClass: "direct-link",
-      permalinkSymbol: "#"
-    }));
-
-//and end here
-
 
 return {
     // When a passthrough file is modified, rebuild the pages:
